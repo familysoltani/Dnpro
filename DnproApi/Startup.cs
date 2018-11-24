fbe55c9d87cbd7e9b5fdf391ad40626d26530ec9
Soltani <family.soltani@gmail.com>
2018-11-25 00:34:38 +0330

    Git Test

--- DnproApi/Startup.cs
+++ DnproApi/Startup.cs
@@ -37,10 +37,10 @@ namespace DnproApi
             }
             else
             {
-                app.UseHsts();
+//                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+//            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
